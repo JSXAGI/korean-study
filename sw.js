ea7f46e3d5490261d25a04f3ea089fee521b4ccf
JSXAGI <154603640+JSXAGI@users.noreply.github.com>
2026-04-03 16:57:51 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'korean-app-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './data.js'
+];
+
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// オフライン時はキャッシュから応答
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
